47e194451f438277d6c6c2e73d30a3004385c863
Neil Stuart <neil.stuart11@gmail.com>
2024-05-04 19:14:03 +0100

    Allow not found page in light theme

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,9 +3,9 @@ import "./output.css";
 
 const NotFound = () => {
     return (
-      <div className="flex flex-col items-center justify-center w-screen grow bg-stone-900">
+      <div className="flex flex-col items-center justify-center w-screen grow ">
           <div className="p-4 border border-stone-950 dark:border-stone-50 rounded-2xl">
-          <h2 className="pb-3 font-mono text-xl text-center text-rose-400">Not Found</h2>
+          <h2 className="pb-3 font-mono text-xl text-center text-rose-600 dark:text-rose-400">Not Found</h2>
           <p className="text-sm dark:text-stone-50 text-stone-950">You have reached the end.</p>
           </div>
       </div>
