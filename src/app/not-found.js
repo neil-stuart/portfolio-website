3328b71d4197b58983799194dde68333b12dc005
Neil Stuart <neil.stuart11@gmail.com>
2024-05-04 12:53:00 +0100

    Update text

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,7 @@ const NotFound = () => {
       <div className="flex flex-col items-center justify-center w-screen grow bg-stone-900">
           <div className="p-4 border border-stone-950 dark:border-stone-50 rounded-2xl">
           <h2 className="pb-3 font-mono text-xl text-center text-rose-400">Not Found</h2>
-          <p className="text-sm dark:text-stone-50 text-stone-950">You've reached the end.</p>
+          <p className="text-sm dark:text-stone-50 text-stone-950">You have reached the end.</p>
           </div>
       </div>
     );
